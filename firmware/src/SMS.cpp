bc1beb835ba816c72be7581734df253fdd792f77
otpz35 <205055121+otpz35@users.noreply.github.com>
2025-10-27 10:40:54 -0300

    Modificar librería SMS
    Se añadió el método smsSerial::waitResponse() para esperar que el modem reciba correctamente el comando hasta que devuelva OK o ERROR

--- firmware/src/SMS.cpp
+++ firmware/src/SMS.cpp
@@ -1,12 +1,15 @@
 #include "SMS.h"
 
-#define GETNUM "AT+CNUM"
+#define CONFIGSET "AT&K0;E0;V1;F1\r"
+#define GETNUM "AT+CNUM\r"
 #define TEXTMODE "AT+CMGF="
 #define SEND "AT+CMGS="
 
-smsSerial::smsSerial(short rx, short tx, int baud = 9600){
+smsSerial::smsSerial(short rx, short tx, int baud){
   uart = new SoftwareSerial(rx, tx);
   uart->begin(baud);
+  uart->print(CONFIGSET);
+  waitResponse();
 }
 smsSerial::~smsSerial(){
   delete uart;
@@ -16,7 +19,8 @@ void smsSerial::textMode(short toggle){
   char enable = (toggle = 0)?('0'):('1'); 
   uart->println(TEXTMODE);
   uart->write(enable);
-  uart->println(); 
+  uart->write("\r");
+  waitResponse(); 
 }
 
 String smsSerial::getNumber(){
@@ -30,7 +34,26 @@ String smsSerial::getNumber(){
 
 void smsSerial::sendMessage(String number, String message){
   uart->print(SEND);
-  uart->println("\""+ number+ "\"");
+  uart->print("\""+ number+ "\"\r");
   uart->print(message);
   uart->write(26);
+  waitResponse(5000);
+}
+
+String smsSerial::waitResponse(const int timeout){
+  unsigned long t0 = millis();
+  String response = "";
+  
+  while(millis-t0<timeout){
+    while(uart->available()){
+      char c = uart->read();
+      response+= c;
+      if(response.endsWith("\r\nOK\r\n")||response.endsWith("\r\nERROR\r\n")){
+        return response;
+      } 
+    }
+    yield();
+  }
+
+  return response;
 }
